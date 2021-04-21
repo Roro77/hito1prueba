ffd26e53640de7ed25afdd77d2305a23ee280ad1
Rodrigo <rodrigo.pineda.a07@gmail.com>
2021-04-21 02:00:50 -0400

    clone twitter desafio

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :tweets
+  devise_for :users, controllers: {registrations: 'users/registrations'}
+  root 'tweets#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
